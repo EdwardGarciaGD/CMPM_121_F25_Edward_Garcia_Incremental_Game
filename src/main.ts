4864679f8755c60233078a6d2cfe52fc3f0649f2
EdwardGarciaGD <egarc216@ucsc.edu>
2025-10-08 22:39:17 +0000

    Added Clicking Button (Step 1 Complete)
    - counter added to record number of clicks
    - counter updates number in webpage

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+//import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+document.body.innerHTML = 
+`
+  <h1>Purple Nurples!</h1>
+  <p>Score: <span id="counter">0</span></p>
 `;
+
+let counter = 0;
+const button = document.createElement("button");
+document.body.appendChild(button);
+
+const counterElement = document.getElementById("counter")!;
+
+button.innerText = "Bake!";
+button.onclick = () =>
+{
+  counter += 1;
+  counterElement.innerText = String(counter);
+};
