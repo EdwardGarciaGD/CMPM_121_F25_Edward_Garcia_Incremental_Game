0651eedbe9369b4f2654e9e0aa84039a320dad9a
EdwardGarciaGD <egarc216@ucsc.edu>
2025-10-10 21:43:33 +0000

    Added Auto Incrementing (Step 3 Complete)
    -Made a function for incrementing and updating text

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,21 @@ document.body.innerHTML = `
   <div id="counter">0 purple nurples</div>
   <br><br>
 `;
-let counter: number = 0;
-const button = document.createElement("button");
-document.body.appendChild(button);
 
+let counter: number = 0;
 const counterElement = document.getElementById("counter")!;
 
+const button = document.createElement("button");
+document.body.appendChild(button);
 button.innerText = "🥧 Bake!";
 button.onclick = () => {
+  updateCounter();
+};
+
+setInterval(updateCounter, 1000);
+
+function updateCounter()
+{
   counter += 1;
   counterElement.textContent = counter + " purple nurples";
-};
+} 
